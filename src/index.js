8181f92630483c687313b571c785991bd21babbe
Egorov Roman <r.egorov@vbankcenter.ru>
2019-10-07 15:17:33 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let result = 0;
+  let factors = expression.split('*');
+  let oddNumbers = 0;
+  for (let i = 0; i < factors.length; i++) {
+    let limit = parseInt(factors[i]);
+    let operation = factors[i].replace(limit, '');
+    // про алгоритм для факториала прочитал на сайте http://javabypatel.blogspot.com/2017/05/count-trailing-zeros-in-factorial-of-number.html
+    if (operation == '!') {
+      let divider = 5;
+      while (limit / divider >= 1) {
+        result += Math.floor(limit / divider);
+        divider *= 5;
+      }
+    // в этом случае алгоритм для факториала не подходит
+    } else if (operation == '!!') { 
+      if (limit % 2 != 0) oddNumbers++;
+      while (limit >= 5) {
+        let divider = 5;
+        // нужно проверять делимость на 5/25 и т.д. каждого множителя
+        while ((limit % divider) == 0) {
+          result++;
+          divider *= 5;
+        }
+        limit -= 2;
+      }
+    }
+  }
+  // если вдруг все исходные числа нечетные, то нулей в сумме на конеце числа не получится (не сможем 5 помножить на 2)
+  return oddNumbers == factors.length ? 0 : result;
 }
